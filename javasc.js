4eed61079388823d8fb0fafe1f4d015cdb150798
damisxx <57136215+damisxx@users.noreply.github.com>
2019-11-26 11:44:13 +0100

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,16 @@
+
+
+function srodek() {
+var tekst="";
+var n ;
+ for(var i=0;i<10;i++){
+	for(var j=0;j<10;j++){
+	tekst + = '<div class ="pole">+i+;
+									
+	
+	
+	
+	}
+	}
+					"</div>";
+	}
